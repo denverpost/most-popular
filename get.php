423972d685eb3c21cbc2c2c14b531b5a896e428d
freejoe76 <jmurphy@denverpost.com>
2015-12-11 17:05:09 -0500

    First stab at the article headline lookup service

---
+++ get.php
@@ -0,0 +1,25 @@
+<?php
+$article = article_lookup(intval($_GET['id']));
+echo $article['title'];
+
+
+function article_lookup($article_id, $return='title')
+{
+    // Pull an article, take its pieces. Sometimes we just want the title, sometimes we want more.
+    $url = 'http://www.mercurynews.com/portlet/article/html/fragments/print_article.jsp?articleId=' . $article_id . '&amp;siteId=36';
+    $content = get_content($url);
+    preg_match('/<td class="articleTitle">([^<]+)<\/td>/', $content, $matches);
+    $article['title'] = $matches[1];
+    if ( $return == 'title' ) return $matches[1];
+    // Line-endings in regex's are the tool of the devil.
+    $content = str_replace("\n", '', $content);
+    $content = str_replace('&', '&amp;', $content);
+    preg_match('/<td class="articleBody">(.*)<\/td><\/tr><tr><td>/', $content, $matches);
+    if ( count($matches) == 0 ) 
+    {
+        return false;
+    }
+    $article['body'] = $matches[1];
+    return $article;
+}
+
