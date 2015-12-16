7f4dcc605e98d1179038e2cbb2ad1e7ce2055784
freejoe76 <jmurphy@denverpost.com>
2015-12-16 17:12:28 -0500

    First draft of h1 class

--- h1.py
+++ h1.py
@@ -5,17 +5,49 @@ import sys
 import doctest
 import string
 import argparse
+import httplib2
+import re
 
+
+class h1:
+    """ """
+
+    def __init__(self):
+        self.options = None
+
+    def set_options(self, options):
+        """ Set the objects options var.
+            """
+        self.options = options
+        return options
+
+    def request(self, url, action='GET', headers={}, request_body=''):
+        """ Download the asset.
+            """
+        h = httplib2.Http('')
+        response, content = h.request(url, action, headers=headers, body=request_body)
+        if response.status > 299:
+            print 'ERROR: HTTP response %s' % response.status
+            sys.exit(1)
+        return content
+
+    def extract(self, element):
+        """ Return the first matching element.
+            """
+        pass
 
 def main(args):
     """ 
         Example command:
-        $ python h1.py http://www.denverpost.com/
+        $ python h1.py --url http://www.denverpost.com/ h1 title meta.robots
         """
     if args:
-        sheet = Sheet('popular')
-        sheet.set_options(args)
-        for worksheet in args.sheets[0]:
+        extract = h1()
+        extract.set_options(args)
+        print args
+        #extract.content = extract.request
+        for element in args.elements[0]:
+
             sheet.worksheet = sheet.open_worksheet(worksheet)
             sheet.fix()
 
@@ -24,7 +56,8 @@ if __name__ == '__main__':
                                      description='',
                                      epilog='')
     parser.add_argument("-v", "--verbose", dest="verbose", default=False, action="store_true")
-    parser.add_argument("urls", action="append", nargs="*")
+    parser.add_argument("-u", "--url", dest="url", default=False)
+    parser.add_argument("elements", action="append", nargs="*")
     args = parser.parse_args()
 
     if args.verbose:
